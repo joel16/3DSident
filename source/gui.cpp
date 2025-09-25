811b6b74475570c64d0ad1ae076ea29032e63dcb
fangrong <darkbluesky@outlook.com>
2025-09-25 18:50:31 +0800

    Revert home btn checking method

--- source/gui.cpp
+++ source/gui.cpp
@@ -378,7 +378,8 @@ namespace GUI {
             
             GUI::DrawText(90, 138, 0.45f, guiButtonTesterText, "Press L + R to return.");
 
-            aptCheckHomePressRejected() ? GUI::DrawImageBlend(btnHome, 180, 215, guiSelectorColour) : GUI::DrawImage(btnHome, 180, 215);
+            SystemStateInfo info = Service::GetSystemStateInfo();
+            ((info.rawButtonState >> 1) & 1) == 0? GUI::DrawImageBlend(btnHome, 180, 215, guiSelectorColour): GUI::DrawImage(btnHome, 180, 215);
 
             kHeld & KEY_L? GUI::DrawImageBlend(btnL, 0, 0, guiSelectorColour) : GUI::DrawImage(btnL, 0, 0);
             kHeld & KEY_R? GUI::DrawImageBlend(btnR, 345, 0, guiSelectorColour) : GUI::DrawImage(btnR, 345, 0);
